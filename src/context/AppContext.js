e291edc76acd10c38c04bc0542267727eb50e9b0
Ricardo Lima <navalimaricardo@gmail.com>
2022-06-21 16:07:43 -0500

    Definiendo primeros valores para mostrar mensaje y ocultarlo

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,4 +1,4 @@
-import React, { createContext} from 'react';
+import React, { createContext, useState} from 'react';
 
 // axios
 import clientAxios from '../axios/axios';
@@ -8,11 +8,20 @@ export const AppContext = createContext();
 
 const AppProvider = props => {
 
+    // state para ocultar el mensaje de bienvenida
+  const [showMessage, setShowMessage] = useState(true);
+
+  // state para mostrar u ocultar formulario de preguntas
+  const [questions, setQuestions] = useState(false);
+
     // valores para acceder por useContext
     return (
         <AppContext.Provider
             value={{
-                hola:"hola"
+                showMessage: showMessage,
+                setShowMessage: setShowMessage,
+                questions: questions,
+                setQuestions
             }}
         >
             { props.children }
