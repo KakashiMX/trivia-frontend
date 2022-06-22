069d81aa18e0ea7acb78cdd0a40830f6abaae870
Ricardo Lima <navalimaricardo@gmail.com>
2022-06-22 13:44:57 -0500

    Definiendo funcion para consultar DB, state para formulario y loading

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,6 +2,7 @@ import React, { createContext, useEffect, useState} from 'react';
 
 // helpers
 import { getCategories } from '../helper/getCategories';
+import { getQuestions } from '../helper/getQuestions';
 
 // creación del context
 export const AppContext = createContext();
@@ -17,6 +18,26 @@ const AppProvider = props => {
   // state para categorias
   const [categories, setCategories] = useState(null);
 
+  // state para el formualario
+  const [formValues, setFormValues] = useState({
+    category: 'aleatoria',
+    total: '',
+    });
+
+    const { category, total } = formValues;
+
+  // state para mostrar un loading
+  const [loading, setLoading] = useState(false);
+
+  // state para las preguntas de la DB
+  const [dataQuestions, setDataQuestions] = useState(null);
+
+  // función para obtener las preguntas
+  const getQuestionsData = async () => {
+    const data = await getQuestions( category, total );
+    setDataQuestions( data );
+  }
+
   useEffect( () => {
 
     const getCategoriesData = async () => {
@@ -25,7 +46,15 @@ const AppProvider = props => {
     }
 
     getCategoriesData();
-  }, []);
+
+    if( loading ) getQuestionsData();
+
+    setTimeout(() => {
+        setLoading( false );
+      }, 3000);
+
+      // eslint-disable-next-line
+  }, [ loading ]);
 
     // valores para acceder por useContext
     return (
@@ -35,7 +64,12 @@ const AppProvider = props => {
                 setShowMessage,
                 questions,
                 setQuestions,
-                categories
+                categories,
+                formValues, 
+                setFormValues,
+                loading,
+                setLoading,
+                dataQuestions
             }}
         >
             { props.children }
