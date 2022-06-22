39fa0a4d51214f2fe5260bf68989894636c66c28
Ricardo Lima <navalimaricardo@gmail.com>
2022-06-22 17:34:24 -0500

    paginacion de preguntas, eliminacion de clase, actualizando respuestas correctas e incorrectas

--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 
 // context
 import { AppContext } from '../../context/AppContext';
@@ -8,40 +8,60 @@ const Questions = () => {
 
     const { category, total } = formValues;
 
+    const optionsRef = useRef();
     // state para hacer una paginación con las preguntas
     const [pagination, setPagination] = useState(0);
 
+    // state para ver cuantas respuestas correctas o incorrectas tiene el usuario
     const [result, setResult] = useState({
         corrects: 0,
         incorrects: 0
     });
+    const { corrects, incorrects } = result;
 
+    // state para mostrar botón de siguiente pregunta
     const [showNext, setShowNext] = useState(false);
 
     // función para actualizar la respuesta seleccionada
     const handleSelect = e => {
-        if( e.target.textContent === dataQuestions[pagination].correct){
-            e.target.classList.add('question__correct');
+
+        // elemento seleccionado
+        const element = e.target;
+
+        // si el contenido del elemento seleccionado es igual a la respuesta correcta
+            // entonces se actualiza el state de respuestas correctas
+        if( element.textContent === dataQuestions[pagination].correct){
+                // se agrega la clase 
+            element.classList.add('question__correct');
             setResult({
                 ...result,
-                corrects: ( result.corrects + 1 )
+                corrects: corrects + 1 
             });
 
+        // si el contenido del elemento seleccionado no es igual a la respuesta correcta
+            // entonces se actualiza el state de respuestas incorrectas
         }else {
-            e.target.classList.add('question__incorrect');
+                // se agrega la clase
+            element.classList.add('question__incorrect');
             setResult({
                 ...result,
-                incorrects: ( result.corrects + 1 )
+                incorrects: incorrects + 1 
             });
         }
-
+        // se actualiza el state del botón para mostrarlo
         setShowNext( true );
     }
 
     // función para pasar a la siguiente pregunta
     const handleNextQuestion = () => {
-        console.log('Siguiente pregunta');
+        setPagination( pagination + 1 );
+        setShowNext( false );
+            // obtenemos las opciones
+        const options = optionsRef.current.children;
+            // convertimos las opciones a arreglo y después eliminamos la clase si es que la tiene
+        Array.from(options).map( option => option.className.includes('question__correct') ? option.classList.remove('question__correct') : option.classList.remove('question__incorrect'));
     }
+
     return (
         <div className='questions__container'>
             
@@ -52,10 +72,13 @@ const Questions = () => {
                     <p className='question__header--question'>{ dataQuestions[pagination].question}</p>
                 </div>
 
-                <div className='question__options'>
+                <div 
+                    className='question__options'
+                    ref={ optionsRef }
+                >
                     { dataQuestions[pagination].options.map( option => 
                         <button
-                            className='question__option'
+                            className="question__option"
                             onClick={ handleSelect }
                             disabled={ showNext }
                         >
