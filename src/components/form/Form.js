10cd02d6814aa1f0b5fe291a9449ffef8b33b10e
Ricardo Lima <navalimaricardo@gmail.com>
2022-06-22 13:45:25 -0500

    mostrando las categorias, accediendo al context

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,23 +4,46 @@ import { AppContext } from '../../context/AppContext';
 const Form = () => {
 
     // accediendo al context
-    const { categories } = useContext( AppContext );
-    
-    console.log( categories )
+    const { categories, setLoading, formValues, setFormValues } = useContext( AppContext );
+
+    const { category, total } = formValues;
+
+    // función para actualizar el valor de los inputs
+    const handleInputChange = e => {
+        setFormValues({
+            ...formValues,
+            [ e.target.name ] : e.target.value
+        });
+    }
+
+    // función para iniciar la trivia
+    const handleInit = e => {
+        e.preventDefault();
+        setLoading( true );
+    }
+
     return (
         <div className='form__container'>
                 <div 
                     className='form__title'>Antes de comenzar</div>
-            <form className='form'>
+            <form 
+                className='form'
+                onSubmit={ handleInit }
+            >
 
                 <div className="form__group">
                     <label htmlFor="" className="form__label">Selecciona una categoria:</label>
-                    <select className='form__input'>
+                    <select 
+                        className='form__input'
+                        name='category'
+                        value={ category }
+                        onChange={ handleInputChange }
+                    >
                         { categories !== null  ?
                             categories.map( category =>
                                 <option 
-                                    value={category.name}
-                                    key={ category.name }
+                                    value={category.nameValue}
+                                    key={ category.nameValue }
                                 >{ category.name}
                                 </option>
                             )
@@ -34,7 +57,11 @@ const Form = () => {
                     <input 
                         type="number" 
                         className="form__input" 
+                        name='total'
+                        value={ total }
+                        onChange={ handleInputChange }
                     />
+                    <small className='form__legend'>Recuerda que el máximo de preguntas es 50</small>
                 </div>
 
                 <input 
